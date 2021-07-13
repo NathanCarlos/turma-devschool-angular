3877c05bdeb021096977e8ae4ba1abb6c0961f22
Nathan Carlos <nathan324116@gmail.com>
2021-07-13 20:44:59 -0300

    feat(projeto-aula-modulos): adiciona projeto

---
+++ projeto-aula-modulos/src/app/features/products/products.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+
+
+@NgModule({
+  declarations: [ProductListComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [ProductListComponent]
+})
+export class ProductsModule { }
